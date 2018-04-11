0b14584704019d69b5c742e24f74b207458cd2f1
fimbifoi <fimbi3rd@gmail.com>
2018-04-11 21:17:27 +0100

    final web page done by Fimbi Foi with wireframes and psd and images

---
+++ script.js
@@ -0,0 +1,70 @@
+var landingMessages = [" De PROXIMITE", " De QUALITE", " D'EXPERTISE", " D'INTEGRITE"],
+    counter = 0,
+    target = document.getElementById("landingMessage");
+
+setInterval(function(){ 
+  
+  target.innerHTML = landingMessages[counter];
+  counter++;
+  
+  if( counter >= landingMessages.length ) {
+    counter = 0;
+  }
+
+}, 1000);
+
+$(document).ready(function(){
+	$(".afro-display-center").slideDown("slow");
+});
+
+
+
+
+// scroll to top icon
+$(document).ready(function(){
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 300) {
+			$('.afro-scroll-icon').fadeIn();
+		} else {
+			$('.afro-scroll-icon').fadeOut();
+			}
+	});
+	
+	//Click event to scroll to top
+	$('.afro-scroll-icon').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	
+});
+// end of scroll function
+
+// $(document).ready(function(){
+// 	$(window).scroll(function(){
+// 		if ($(this).scrollTop() > 300) {
+// 			$('.scrollToTop').fadeIn();
+// 		} else {
+// 			$('.scrollToTop').fadeOut();
+// 		};
+// 	});
+// });
+
+// (document).ready(function(){
+
+//     //Check to see if the window is top if not then display button
+//     $(window).scroll(function(){
+//         if ($(this).scrollTop() > 100) {
+//             $('.afro-scroll-icon').fadeIn();
+//         } else {
+//             $('.afro-scroll-icon').fadeOut();
+//         }
+//     });
+
+//     //Click event to scroll to top
+//     $('.afro-scroll-icon').click(function(){
+//         $('html, body').animate({scrollTop : 0},800);
+//         return false;
+//     });
+
+// });
